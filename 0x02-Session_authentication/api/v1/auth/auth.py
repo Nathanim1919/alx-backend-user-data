073b0b341d9d969d7f2e003c8234e4fd6d58cf30
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-27 15:09:45 +0300

    Empty session

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -13,13 +13,20 @@ class Auth:
         """
         if path is None:
             return True
-
-        if excluded_paths is None or not excluded_paths:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization Header"""
