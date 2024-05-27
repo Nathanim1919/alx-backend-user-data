40cae568b6b807842ddc77244ea44cd2e0557050
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-27 16:19:29 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Session authentication module
 """
-import uuid
+from uuid import uuid4
 from .auth import Auth
 
 
@@ -15,9 +15,9 @@ class SessionAuth(Auth):
         """
         if user_id is None or type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+        session_id = uuid4()
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
     
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
