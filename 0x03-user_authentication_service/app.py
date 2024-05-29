2ab34da3cfca8c67dbb5c12a2ced3c9f10113162
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-29 16:36:54 +0300

    implement login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Main file"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, request, abort
 from auth import Auth
 
 
@@ -32,6 +32,21 @@ def users() -> str:
     # and return a 400 status code
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    email, password = request.form.get('email'), request.form.get('password')
+    if (auth.valid_login(email, password)):
+        session_id = auth.create_session(email)
+        # store the session id as a cookie with key "session_id"
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        # Respond with the following JSON payload:
+        # {"email": "<registered email>", "message": "logged in"}
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == '__main__':
