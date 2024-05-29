8f8266a4553111723cf2655f4042461bbf205c76
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-29 14:52:25 +0300

    create flask end point for user registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,17 +14,22 @@ def index():
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def users():
-    """Create a new user
-        Returns:
-            json: user created message
+def users() -> str:
+    """POST /users
+    Return:
+        - JSON payload of the form containing various information.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-
+    # Get the email and password from form data
+    email, password = request.form.get("email"), request.form.get("password")
     try:
+        # Register the user
         auth.register_user(email, password)
-        return jsonify({"email":email,"message": "user created"}), 201
+        # Respond with the following JSON payload:
+        # {"email": "<registered email>", "message": "user created"}
+        return jsonify({"email": email, "message": "user created"})
+    # If the user is already registered, catch the exception and return a
+    # JSON payload of the form: {"message": "email already registered"}
+    # and return a 400 status code
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
