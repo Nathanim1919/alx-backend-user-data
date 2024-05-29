dcb682687c0072288a7b664127b1f557c22333e0
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-29 10:27:37 +0300

    change string to varchar for column decalration

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """
 User Model Module
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -13,7 +13,7 @@ class User(Base):
     """ User Model """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(255), nullable=False)
+    session_id = Column(VARCHAR(255), nullable=True)
+    reset_token = Column(VARCHAR(255), nullable=True)
