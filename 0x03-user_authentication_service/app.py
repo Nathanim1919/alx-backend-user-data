dbf7d45019302abb9c960b086ffe93ff0899f699
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-29 14:44:29 +0300

    create flask end point for user registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,17 +13,21 @@ def index():
     return jsonify({'message': 'Bienvenue'})
 
 
-@app.route('/users', methods=['POST'])
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
+    """Create a new user
+        Returns:
+            json: user created message
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
     try:
         auth.register_user(email, password)
+        return jsonify({"email":email,"message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-    return jsonify({"email":email,"message": "user created"}), 201
 
 
 if __name__ == '__main__':
